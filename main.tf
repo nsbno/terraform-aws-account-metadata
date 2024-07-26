ea2366ebf6ce088dc732d5226f4da0852f3d1adb
Nicolas Harlem Eide <nicolas@harlemeide.net>
2024-07-26 11:57:46 +0200

    Create the module with dns, network, lb and account data

--- main.tf
+++ main.tf
@@ -1,10 +1,87 @@
-terraform {
-  required_version = ">= 1.3.0"
+data "aws_caller_identity" "this" {}
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0.0"
-    }
+/*
+ * == Domain Name
+ */
+data "aws_iam_account_alias" "this" {}
+
+locals {
+  # The account alias includes the name of the environment we are in as a suffix
+  split_alias = split("-", data.aws_iam_account_alias.this.account_alias)
+
+  environment_index = length(local.split_alias) - 1
+
+  account_name = join("-", slice(local.split_alias, 0, length(local.split_alias) - 1))
+  environment  = local.split_alias[local.environment_index]
+
+  is_production = local.environment == "prod"
+
+  account_base_domain = "${local.account_name}.vydev.io"
+  account_domain      = local.is_production ? local.account_base_domain : "${local.environment}.${local.account_base_domain}"
+}
+
+data "aws_route53_zone" "this" {
+  name = local.account_domain
+}
+
+/*
+ *  == Network Information
+ */
+
+data "aws_vpc" "shared" {
+  filter {
+    name   = "tag:Name"
+    values = ["shared"]
   }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.shared.id]
+  }
+
+  filter {
+    name   = "tag:Tier"
+    values = ["Private"]
+  }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.shared.id]
+  }
+
+  filter {
+    name   = "tag:Tier"
+    values = ["Public"]
+  }
+}
+
+
+/*
+ * == Load Balancer
+ */
+
+data "aws_lbs" "all" {}
+
+data "aws_lb" "all" {
+  for_each = data.aws_lbs.all.arns
+
+  arn = each.value
+}
+
+locals {
+  # Get the ARN of the load balancer that is in the same VPC as the shared VPC
+  lbs_in_shared_vpc = [
+    for lb in data.aws_lb.all : lb.arn if lb.vpc_id == data.aws_vpc.shared.id
+  ]
+  # Then we just assume that there is only one load balancer in the shared VPC
+  lb_arn = local.lbs_in_shared_vpc[0]
+}
+
+data "aws_lb_listener" "https" {
+  load_balancer_arn = local.lb_arn
+  port              = 443
 }
---
+++ outputs.tf
@@ -0,0 +1,49 @@
+output "account" {
+  value = {
+    account_id = data.aws_caller_identity.current.account_id
+
+    account_name = local.account_name
+    environment  = local.environment
+  }
+}
+
+/*
+ * == Domain Name
+ */
+
+output "dns" {
+  value = {
+    domain_name    = local.account_domain
+    hosted_zone_id = data.aws_route53_zone.this.zone_id
+  }
+}
+
+/*
+ * == Network Information
+ */
+
+output "network" {
+  value = {
+    vpc_id             = data.aws_vpc.shared.id
+    private_subnet_ids = data.aws_subnets.private.ids
+    public_subnet_ids  = data.aws_subnets.public.ids
+  }
+}
+
+/*
+ * == Load Balancer
+ */
+
+output "load_balancer" {
+  value = {
+    arn        = local.lb_arn
+    arn_suffix = data.aws_lb.all[local.lb_arn].arn_suffix
+
+    dns_name = data.aws_lb.all[local.lb_arn].dns_name
+    zone_id  = data.aws_lb.all[local.lb_arn].zone_id
+
+    https_listener_arn = data.aws_lb_listener.https.arn
+
+    security_group_id = tolist(data.aws_lb.all[local.lb_arn].security_groups)[0]
+  }
+}
